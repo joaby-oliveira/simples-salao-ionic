78615fe672dac1d915993366187d3443bb57cc5d
Joaby <joabyoliveira2004@gmail.com>
2023-11-15 14:19:45 -0300

    feat: structure home design

--- src/app/features/home/container/home-root/home-root.component.ts
+++ src/app/features/home/container/home-root/home-root.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './home-root.component.html',
-  styleUrls: ['./home-root.component.css']
+  styleUrls: ['./home-root.component.css'],
 })
 export class HomeRootComponent {
-
+  userName = 'John Doe';
 }
--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeRootComponent } from './container/home-root/home-root.component';
+import { IonicModule } from '@ionic/angular';
 
 const routes: Routes = [{ path: '', component: HomeRootComponent }];
 
 @NgModule({
   declarations: [HomeRootComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), IonicModule],
 })
 export class HomeModule {}
