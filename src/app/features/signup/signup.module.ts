fccd1faf75ff4ad6130cd7383d658dc03a6de006
Joaby <joabyoliveira2004@gmail.com>
2023-11-15 08:34:41 -0300

    feat: add form header to signup page.

--- src/app/features/signup/signup.module.ts
+++ src/app/features/signup/signup.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { SignupRootComponent } from './containers/signup-root/signup-root.component';
 import { IonicModule } from '@ionic/angular';
+import { FormHeaderComponent } from 'src/app/global/components/form-header/form-header.component';
 
 const routes: Routes = [{ path: '', component: SignupRootComponent }];
 
 @NgModule({
   declarations: [SignupRootComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), IonicModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    IonicModule,
+    FormHeaderComponent,
+  ],
 })
 export class SignupModule {}
