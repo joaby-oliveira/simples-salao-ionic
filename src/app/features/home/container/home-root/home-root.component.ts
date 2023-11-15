637f4e59f69da7fd3aaf9c1db7e0c205fc92dbc0
Joaby <joabyoliveira2004@gmail.com>
2023-11-15 14:25:30 -0300

    Add upcoming appointments and recommended services to home page

--- src/app/features/home/container/home-root/home-root.component.ts
+++ src/app/features/home/container/home-root/home-root.component.ts
@@ -6,4 +6,26 @@ import { Component } from '@angular/core';
 })
 export class HomeRootComponent {
   userName = 'John Doe';
+  appointments = [
+    {
+      serviceName: 'Corte de cabelo',
+      price: 50,
+      date: new Date(2022, 1, 20, 10, 30),
+    },
+    {
+      serviceName: 'Barba',
+      price: 30,
+      date: new Date(2022, 1, 21, 14, 0),
+    },
+  ];
+  recommendedServices = [
+    {
+      name: 'Corte de cabelo',
+      price: 50,
+    },
+    {
+      name: 'Barba',
+      price: 30,
+    },
+  ];
 }
